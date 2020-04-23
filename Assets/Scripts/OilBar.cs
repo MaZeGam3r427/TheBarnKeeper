03b06643e9ad3161d778b071bdf6beef130b343c
joseph pelloquet <joseph.pelloquet@gmail.com>
2020-04-23 20:14:54 +0200

    Test pour travail et corretion

--- Assets/Scripts/OilBar.cs
+++ Assets/Scripts/OilBar.cs
@@ -12,19 +12,23 @@ public class OilBar : MonoBehaviour
 
     void Update()
     {
+        //permet de remplire la barre d'huile sans dépaser de zéro a cent 
         oilBarImage.fillAmount = Oil / MaxOil;
         Oil = Mathf.Clamp(Oil, 0f, MaxOil);
 
+        //touche temporaire pour remplir la barre d'huile
         if(Input.GetKeyDown(KeyCode.E))
         {
             Oil = Oil + 10;
         }
 
+        //touche pour activer la lanterne et la consommation d'huile
         if(Input.GetKeyDown(KeyCode.A))
         {
             Oil = Oil - 10;
         }
 
+        //permet de désactiver la lanterne lorsque la barre atteint zéro
         if (Oil == 0)
         {
             Light.SetActive(false);

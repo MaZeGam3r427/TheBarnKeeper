11f419ffd0c5f4dd1afbe73605d98a95ec379520
MaZeGam3r427 <samuelarsac@hotmail.com>
2020-04-24 12:00:55 +0200

    Enjambement quasiment fonctionnel

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -1,17 +1,40 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Cinemachine;
 
 public class PlayerMovement : MonoBehaviour
 {
-
-    private bool touchingCollider = false;
-
     public Animator Myanim;
 
-
+    [Header("Character")]
     public CharacterController controller;
+    public GameObject Player;
 
+    int WayClimb = 1;
+    float climbTimer;
+    bool isClimbing;
+    bool canClimbing;
+    bool lookAt;
+    bool canWalk = true;
+    public static bool useRaycast;
+    [HideInInspector] public Raycast myRaycast;
+    public GameObject LanternLight;
+
+    [Header("Camera")]
+    public CinemachineVirtualCamera VirtualCam1;
+    public CinemachineVirtualCamera VirtualCam2;
+    public CinemachineVirtualCamera VirtualCam3;
+
+    [Header("WayPoints")]
+    public GameObject WayPoint1;
+    public GameObject WayPoint2;
+
+    [Header("Climbing")]
+    public GameObject CamMovment;
+    public GameObject ClimbText;
+
+    [Header("Stats")]
     public float speed = 12f;
     public float gravity = -9.81f;
 
@@ -19,6 +42,10 @@ public class PlayerMovement : MonoBehaviour
 
     private void Start()
     {
+        useRaycast = true;
+        myRaycast = GameObject.Find("VCam1").GetComponent<Raycast>();
+        Cursor.visible = false;
+        Cursor.lockState = CursorLockMode.Locked;
     }
 
 
@@ -27,43 +54,115 @@ public class PlayerMovement : MonoBehaviour
         float x = Input.GetAxis("Horizontal");
         float z = Input.GetAxis("Vertical");
 
-        Vector3 move = transform.right * x + transform.forward * z;
-
-        controller.Move(move * speed * Time.deltaTime);
-
-        velocity.y += gravity * Time.deltaTime;
-
-        controller.Move(velocity * Time.deltaTime);
-
-
-        if (Input.GetKeyDown(KeyCode.F))
+        if(canWalk)
         {
-            if (touchingCollider == true)
+            Vector3 move = transform.right * x + transform.forward * z;
+
+            controller.Move(move * speed * Time.deltaTime);
+
+            velocity.y += gravity * Time.deltaTime;
+
+            controller.Move(velocity * Time.deltaTime);
+        }
+
+        lookAt = Raycast.isLooking;
+        ClimbText.SetActive(false);
+
+        //Augmente le timer si on enjambe l'obstalce
+        if (isClimbing)
+        {
+            LanternLight.SetActive(false);
+            climbTimer += Time.deltaTime;
+        }
+
+        //Si le joueur observe l'obstacle, le joueur peut enjamber
+        if (lookAt)
+        {
+            ActivateClimb();
+        }
+        else
+        {
+            DeactivateClimb();
+        }
+
+        //Si on appuie sur E et que l'on peut enjamber l'obstacle
+        if (Input.GetKeyDown(KeyCode.F) && canClimbing)
+        {
+            Player.gameObject.SetActive(false);
+            CamMovment.GetComponent<CameraMovement>().enabled = false;
+            canWalk = false;
+            isClimbing = true;
+            useRaycast = false;
+
+            //Si l'on vient de la droite, change la priorité sur la première caméra
+            if (WayClimb > 0)
             {
-                Myanim.SetTrigger("Climbing");
+                VirtualCam2.Priority = 15;
+            }
+            //Sinon change la priorité sur la deuxième caméra
+            else
+            {
+                VirtualCam3.Priority = 15;
             }
         }
+
+        if (climbTimer > 0)
+        {
+            DeactivateClimb();
+        }
+
+        if(climbTimer == 0f)
+        {
+            LanternLight.SetActive(true);
+        }
+
+        //Si le timer d'enjambement est plus grand que 0 et si l'on vient de la gauche
+        if (climbTimer >= 1f && WayClimb > 0)
+        {
+            gameObject.transform.position = WayPoint2.transform.position;
+            VirtualCam2.Priority = 5;
+        }
+
+        //Si le timer d'enjambement est plus petit que 0 et si l'on vient de la droite
+        if (climbTimer >= 1f && WayClimb < 0)
+        {
+            gameObject.transform.position = WayPoint1.transform.position;
+            VirtualCam3.Priority = 5;
+        }
+
+        //s'éxécute quand l'enjambement est fini
+        if (climbTimer >= 2f)
+        {
+            Player.gameObject.SetActive(true);
+            CamMovment.GetComponent<CameraMovement>().enabled = true;
+            canWalk = true;
+            isClimbing = false;
+            useRaycast = true;
+
+            climbTimer = 0f;
+
+            if (WayClimb < 0)
+            {
+                WayClimb = 1;
+            }
+            else
+            {
+                WayClimb = -1;
+            }
+        }
+
     }
 
-    void OnTriggerEnter(Collider other)
+    void ActivateClimb()
     {
-        if (other.tag == "Obstacle")
-        {
-            touchingCollider = true;
-        }
+        canClimbing = true;
+        ClimbText.SetActive(true);
     }
 
-    /*void OnTriggerExit(Collider other)
+    void DeactivateClimb()
     {
-        if (other.tag == "Obstacle")
-        {
-            touchingCollider = false;
-        }
-    }*/
-
-    public void TouchingColliderOff()
-    {
-        touchingCollider = false;
+        canClimbing = false;
+        ClimbText.SetActive(false);
     }
 
 }

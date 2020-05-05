c27658dc0a8c4a83788d8d6dfbbb1d11db8c5970
Healing Storm <vincentpy0902@gmail.com>
2020-05-05 12:08:20 +0200

    Cadenas à chiffre ajouté

--- Assets/Scripts/Player/LockSystem.cs
+++ Assets/Scripts/Player/LockSystem.cs
@@ -8,7 +8,9 @@ public class LockSystem : MonoBehaviour
 {
     //objet à faire apparaitre/disparaitre
     public GameObject StorageLockMinigame;
-    public GameObject ReturnGame;
+    public GameObject DesktopLockMinigame;
+    public GameObject ReturnGameStorage;
+    public GameObject ReturnGameDesktop;
 
     //Emplacements des lettres (gameobjects) et numéro de l'array (ints) pour la remise
     public GameObject StorageLockSlot1;
@@ -21,15 +23,30 @@ public class LockSystem : MonoBehaviour
     private int LetterStorageLockSlot4 = 0;
     //
     //Emplacements des lettres (gameobjects) et numéro de l'array (ints) pour le bureau
-
+    public GameObject DesktopLockSlot1;
+    private int NumberDesktopLockSlot1 = 0;
+    public GameObject DesktopLockSlot2;
+    private int NumberDesktopLockSlot2 = 0;
+    public GameObject DesktopLockSlot3;
+    private int NumberDesktopLockSlot3 = 0;
+    public GameObject DesktopLockSlot4;
+    private int NumberDesktopLockSlot4 = 0;
+    //
 
     public string Alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
     public char[] AlphabetArray;
 
+    public string Numbers = "0123456789";
+    public char[] NumbersArray;
+
     private void Start()
     {
         StorageLockMinigame.SetActive(false);
-        ReturnGame.SetActive(false);
+        DesktopLockMinigame.SetActive(false);
+
+        ReturnGameStorage.SetActive(false);
+        ReturnGameDesktop.SetActive(false);
+
         AlphabetArray = Alphabet.ToCharArray();
         LetterStorageLockSlot1 = 9;
         LetterStorageLockSlot2 = 21;
@@ -39,28 +56,62 @@ public class LockSystem : MonoBehaviour
         StorageLockSlot2.GetComponent<TextMeshProUGUI>().text = AlphabetArray[LetterStorageLockSlot2].ToString();
         StorageLockSlot3.GetComponent<TextMeshProUGUI>().text = AlphabetArray[LetterStorageLockSlot3].ToString();
         StorageLockSlot4.GetComponent<TextMeshProUGUI>().text = AlphabetArray[LetterStorageLockSlot4].ToString();
+
+        NumbersArray = Numbers.ToCharArray();
+        NumberDesktopLockSlot1 = 7;
+        NumberDesktopLockSlot2 = 3;
+        NumberDesktopLockSlot3 = 0;
+        NumberDesktopLockSlot4 = 5;
+        DesktopLockSlot1.GetComponent<TextMeshProUGUI>().text = NumbersArray[NumberDesktopLockSlot1].ToString();
+        DesktopLockSlot2.GetComponent<TextMeshProUGUI>().text = NumbersArray[NumberDesktopLockSlot2].ToString();
+        DesktopLockSlot3.GetComponent<TextMeshProUGUI>().text = NumbersArray[NumberDesktopLockSlot3].ToString();
+        DesktopLockSlot4.GetComponent<TextMeshProUGUI>().text = NumbersArray[NumberDesktopLockSlot4].ToString();
     }
 
     private void OnTriggerEnter(Collider other)
     {
         if(other.tag == "LockStorage")
         {
-            LockUIEnter();
+            LockStorageUIEnter();
+        }
+
+        if (other.tag == "LockDesktop")
+        {
+            LockDesktopUIEnter();
         }
     }
 
-    public void LockUIEnter()
+    public void LockStorageUIEnter()
     {
         StorageLockMinigame.SetActive(true);
-        ReturnGame.SetActive(true);
+        ReturnGameStorage.SetActive(true);
         Time.timeScale = 0f;
         Cursor.visible = true;
         Cursor.lockState = CursorLockMode.None;
     }
-    public void LockUIExit()
+
+    public void LockDesktopUIEnter()
+    {
+        DesktopLockMinigame.SetActive(true);
+        ReturnGameDesktop.SetActive(true);
+        Time.timeScale = 0f;
+        Cursor.visible = true;
+        Cursor.lockState = CursorLockMode.None;
+    }
+
+    public void LockStorageUIExit()
     {
         StorageLockMinigame.SetActive(false);
-        ReturnGame.SetActive(false);
+        ReturnGameStorage.SetActive(false);
+        Time.timeScale = 1f;
+        Cursor.visible = false;
+        Cursor.lockState = CursorLockMode.Locked;
+    }
+
+    public void LockDesktopUIExit()
+    {
+        DesktopLockMinigame.SetActive(false);
+        ReturnGameDesktop.SetActive(false);
         Time.timeScale = 1f;
         Cursor.visible = false;
         Cursor.lockState = CursorLockMode.Locked;
@@ -194,4 +245,126 @@ public class LockSystem : MonoBehaviour
 
     //DESKTOP
     //SLOT1
+    public void DesktopLockUpSlot1()
+    {
+        if (NumberDesktopLockSlot1 != 26)
+        {
+            NumberDesktopLockSlot1 = NumberDesktopLockSlot1 + 1;
+
+            if (NumberDesktopLockSlot1 == 26)
+            {
+                NumberDesktopLockSlot1 = 0;
+            }
+
+            DesktopLockSlot1.GetComponent<TextMeshProUGUI>().text = NumbersArray[NumberDesktopLockSlot1].ToString();
+        }
+    }
+
+    public void DesktopLockDownSlot1()
+    {
+        if (NumberDesktopLockSlot1 != -1)
+        {
+            NumberDesktopLockSlot1 = NumberDesktopLockSlot1 - 1;
+
+            if (NumberDesktopLockSlot1 == -1)
+            {
+                NumberDesktopLockSlot1 = 25;
+            }
+
+            DesktopLockSlot1.GetComponent<TextMeshProUGUI>().text = NumbersArray[NumberDesktopLockSlot1].ToString();
+        }
+    }
+
+    //SLOT 2
+    public void DesktopLockUpSlot2()
+    {
+        if (NumberDesktopLockSlot2 != 26)
+        {
+            NumberDesktopLockSlot2 = NumberDesktopLockSlot2 + 1;
+
+            if (NumberDesktopLockSlot2 == 26)
+            {
+                NumberDesktopLockSlot2 = 0;
+            }
+
+            DesktopLockSlot2.GetComponent<TextMeshProUGUI>().text = NumbersArray[NumberDesktopLockSlot2].ToString();
+        }
+    }
+
+    public void DesktopLockDownSlot2()
+    {
+        if (NumberDesktopLockSlot2 != -1)
+        {
+            NumberDesktopLockSlot2 = NumberDesktopLockSlot2 - 1;
+
+            if (NumberDesktopLockSlot2 == -1)
+            {
+                NumberDesktopLockSlot2 = 25;
+            }
+
+            DesktopLockSlot2.GetComponent<TextMeshProUGUI>().text = NumbersArray[NumberDesktopLockSlot2].ToString();
+        }
+    }
+
+    //SLOT 3
+    public void DesktopLockUpSlot3()
+    {
+        if (NumberDesktopLockSlot3 != 26)
+        {
+            NumberDesktopLockSlot3 = NumberDesktopLockSlot3 + 1;
+
+            if (NumberDesktopLockSlot3 == 26)
+            {
+                NumberDesktopLockSlot3 = 0;
+            }
+
+            DesktopLockSlot3.GetComponent<TextMeshProUGUI>().text = NumbersArray[NumberDesktopLockSlot3].ToString();
+        }
+    }
+
+    public void DesktopLockDownSlot3()
+    {
+        if (NumberDesktopLockSlot3 != -1)
+        {
+            NumberDesktopLockSlot3 = NumberDesktopLockSlot3 - 1;
+
+            if (NumberDesktopLockSlot3 == -1)
+            {
+                NumberDesktopLockSlot3 = 25;
+            }
+
+            DesktopLockSlot3.GetComponent<TextMeshProUGUI>().text = NumbersArray[NumberDesktopLockSlot3].ToString();
+        }
+    }
+
+    //SLOT 4
+    public void DesktopLockUpSlot4()
+    {
+        if (NumberDesktopLockSlot4 != 26)
+        {
+            NumberDesktopLockSlot4 = NumberDesktopLockSlot4 + 1;
+
+            if (NumberDesktopLockSlot4 == 26)
+            {
+                NumberDesktopLockSlot4 = 0;
+            }
+
+            DesktopLockSlot4.GetComponent<TextMeshProUGUI>().text = NumbersArray[NumberDesktopLockSlot4].ToString();
+        }
+    }
+
+    public void DesktopLockDownSlot4()
+    {
+        if (NumberDesktopLockSlot4 != -1)
+        {
+            NumberDesktopLockSlot4 = NumberDesktopLockSlot4 - 1;
+
+            if (NumberDesktopLockSlot4 == -1)
+            {
+                NumberDesktopLockSlot4 = 25;
+            }
+
+            DesktopLockSlot4.GetComponent<TextMeshProUGUI>().text = NumbersArray[NumberDesktopLockSlot4].ToString();
+        }
+    }
 }

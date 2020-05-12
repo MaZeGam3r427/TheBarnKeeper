56aa0b5d4e01af92cc2e122df45e0ee0cd1c9a1d
joseph pelloquet <joseph.pelloquet@gmail.com>
2020-05-12 17:57:22 +0200

    rajout de mouvement+ fuite

---
+++ Assets/Scripts/Monstre/ScriptDegage.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class ScriptDegage : MonoBehaviour
+{
+    public NavMeshAgent agent;
+    public Transform target;
+    public float distance;
+    public float radius = 3f;
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.A))
+        { 
+            agent.destination = target.position;
+            agent.stoppingDistance = 5f;
+            
+        }
+    }
+}
---
+++ Assets/Scripts/Monstre/ScriptDetection.cs
@@ -0,0 +1,83 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class ScriptDetection : MonoBehaviour
+{
+
+    public Transform cible;//glisser l'objet player
+    private Transform maTransform;
+    private NavMeshAgent agent;
+    public bool poursuite;
+    public float pdv = 10f;
+    public bool pause;
+
+    public float PLS = 5f;
+    public float Fine = 0f;
+
+    void Awake()
+    {
+        maTransform = transform;
+    }
+
+    // Use this for initialization
+    void Start()
+    {
+        Fine = PLS;
+
+        //Initialisation du script NavMeshAgen qui se trouve sur le même objet que ce script
+        agent = GetComponent<NavMeshAgent>();
+
+        pause = false;
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(pause == true)
+        {
+            Fine -= 1 * Time.deltaTime;
+
+        }
+
+        if (Fine <= 0f)
+        {
+            pause = false;
+            Fine = PLS;
+        }
+
+        if (poursuite)
+        {
+            mouvement();
+        }
+
+        if (poursuite == false && pause == true)
+        {
+            miseEnAttente();
+        }
+
+    }
+
+
+    private void mouvement()
+    {
+        //Si la variable "vieActuelle" est supérieur a 0
+        if (pdv > 0)
+        {
+            Debug.DrawLine(cible.transform.position, maTransform.position, Color.blue);
+            agent.destination = cible.position;//le squelette se dirige vers le joueur
+        }
+    }
+
+    //L'ennemi reste a sa position actuelle
+    private void miseEnAttente()
+    {
+        print("NE BOUGE PLUS !!");
+        agent.destination = transform.position;
+    }
+
+
+}
+

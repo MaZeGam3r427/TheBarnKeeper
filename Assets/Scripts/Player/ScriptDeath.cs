ea12903b68c19df9d9bba7f4e30e95862d543509
joseph pelloquet <joseph.pelloquet@gmail.com>
2020-05-27 12:25:00 +0200

    tentative de mort par le monstre

---
+++ Assets/Scripts/Player/ScriptDeath.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScriptDeath : MonoBehaviour
+{
+    public GameObject DeathScreenUI;
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    private void OnCollisionEnter(Collision col)
+    {
+       if(col.gameObject.name == "Group7952")
+        {
+            gameObject.SetActive(false);
+            DeathScreenUI.SetActive(true);
+            Debug.Log("your dead");
+        }
+    }
+}

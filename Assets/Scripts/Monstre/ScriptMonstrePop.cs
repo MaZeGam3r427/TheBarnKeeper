11e4018496758e5126e9b1cee1eda1c7f7d4f71c
joseph pelloquet <joseph.pelloquet@gmail.com>
2020-06-01 01:02:50 +0200

    Monstre apparait et disparait au 1er étage

---
+++ Assets/Scripts/Monstre/ScriptMonstrePop.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScriptMonstrePop : MonoBehaviour
+{
+    public GameObject Monstre;
+    
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.CompareTag("Player"))
+        {
+            Monstre.SetActive(true);
+        }
+    }
+}
